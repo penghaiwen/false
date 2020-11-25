1aad6f5f4d031b352fec32776e2148865e3e1270
1515431459@qq.com <birthday817>
2020-11-25 15:26:07 +0800

    修改打包

--- oauth2/src/main/java/com/sys/service/impl/SysUserServiceImpl.java
+++ oauth2/src/main/java/com/sys/service/impl/SysUserServiceImpl.java
@@ -3,23 +3,18 @@ package com.sys.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.exception.RestException;
-import com.redis.DelayingQueueService;
 import com.security.JwtUser;
 import com.sys.entity.SysUser;
 import com.sys.mapper.SysUserMapper;
 import com.sys.service.ISysUserService;
 import org.springframework.beans.BeanUtils;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 public class SysUserServiceImpl  extends ServiceImpl<SysUserMapper, SysUser> implements ISysUserService {
 
-    @Resource
-    private DelayingQueueService delayingQueueService;
+
     @Override
     public JwtUser getUserByUsername(String userName) {
         SysUser user= baseMapper.getUserByUsername(userName);
